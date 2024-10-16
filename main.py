78b32be5ed7eeed967545986190e07688f1c2f0e
chamrouensann <chamrouensann.edu@gmail.com>
2024-10-16 14:04:31 +0700

    Getting the user input and print out base on the amount of while loop

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Enter your To-Do list: ")
+user_input = input()
+print("Here is your list: ",user_input)
+user_number = int(user_input)
+
+user_amount = 1
+while user_amount <= user_number:
+    print("Here is user amount: ", user_amount)
+    user_amount = user_amount + 1
